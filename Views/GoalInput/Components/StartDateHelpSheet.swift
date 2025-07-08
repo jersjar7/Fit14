1b1431d293f4e8b72dd48edba721dc512dacfaef
Jerson <jerson.7@icloud.com>
2025-07-07 19:18:23 -0600

    Plan Start Date now considered

---
+++ Views/GoalInput/Components/StartDateHelpSheet.swift
@@ -0,0 +1,18 @@
+//
+//  StartDateHelpSheet.swift
+//  Fit14
+//
+//  Created by Jerson on 7/7/25.
+//
+
+import SwiftUI
+
+struct StartDateHelpSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartDateHelpSheet()
+}
